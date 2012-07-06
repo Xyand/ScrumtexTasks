e33b72318e7053e20fd409e95f0a423d73ac3605
Xyand <xyand@fogofcode.com>
2012-07-06 14:25:10 +0300

    Added multi document/multi color per user story (first field in the CSV) Added multiple user stories to the input example

--- MakeTasksTex.py
+++ MakeTasksTex.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict
 
 #TODO: Replace find and replace wit format
 template_slide = r'''
@@ -15,6 +16,10 @@ Assigned to: & Estimate: $estimate & Reviewer:
     \end{frame}
 '''
 
+color_list = ['red', 'green', 'blue', 'black', 
+        'brown', 'orange', 'greenyellow', 
+        'sepia', 'grey', 'magenta']
+
 color_command = r'\usecolortheme[named={color}]{{structure}}'
 list_delim = r'\item '
 
@@ -26,30 +31,50 @@ def expand_itemize(fields, index):
                       list_delim + list_delim.join(items) + \
                       '\\end{itemize}\n'
 
-# Print header
-file_header = open('TaskHeader.tex', 'r')
-for line in file_header:
-    print line
+# TODO: Make it a class
 
-print color_command.format(color='green')
-print r'\begin{document}'
+def make_story(name_story, tasks, color, columns):
+
+    # Print header
+    file_header = open('TaskHeader.tex', 'r')
+    for line in file_header:
+        print line
+
+    # Choose color
+    print color_command.format(color=color)
+
+    # Begin document
+    print r'\begin{document}'
+
+    # Render tasks
+    for fields in tasks:
+        if not len(fields) == len(columns):
+            sys.stderr.write('Field number mismatch\n')
+            sys.exit(1)
+
+        slide = template_slide
+        for i_field in xrange(len(fields)):
+            expand_itemize(fields, i_field);
+            slide = slide.replace('$' + columns[i_field], fields[i_field]);
+
+    print slide
+
+    # Close document
+    print r'\end{document}'
 
 line = sys.stdin.readline()
 columns = map(str.strip, line.split(','))
 
-for line in sys.stdin:
-    fields = map(str.strip, line.split(','))
+task_lines = sys.stdin.readlines()
+grouped_lines = defaultdict(list)
 
-    if not len(fields) == len(columns):
-        sys.stderr.write('Field number mismatch\n')
-        sys.exit(1)
+for line in task_lines:
+    line_split = line.split(',')
+    grouped_lines[line_split[0]].append(line_split)
 
-    slide = template_slide
-    for i_field in xrange(len(fields)):
-        expand_itemize(fields, i_field);
-        slide = slide.replace('$' + columns[i_field], fields[i_field]);
 
-    print slide
+color = 0
+for name_story in grouped_lines:
+    make_story(name_story, grouped_lines[name_story], color_list[color], columns)
+    color = (color + 1) % len(color_list)
 
-# Close document
-print r'\end{document}'
