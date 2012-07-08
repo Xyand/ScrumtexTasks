aab8adaa0fe21cc9f0e0ad330c6ff7d103f0da08
Xyand <xyand@fogofcode.com>
2012-07-08 15:53:31 +0300

    Added main function

--- MakeTasksTex.py
+++ MakeTasksTex.py
@@ -4,8 +4,6 @@ from collections import defaultdict
 from os import mkdir
 from os.path import isdir
 
-#TODO: Replace find and replace with format
-
 color_list = ['red', 'green', 'blue', 'cyan', 'brown', 'orange', 'gray',  'teal', 
               'olive', 'magenta']
 
@@ -46,26 +44,31 @@ def make_story(name_story, tasks, color, columns, template):
     tex_out.write('\\end{document}\n')
     tex_out.close()
 
-# Read frame template
-frame_template_file = open('./FrameTemplate.tex','r')
-frame_template = Template(frame_template_file.read())
+def main():
+    # Read frame template
+    frame_template_file = open('./FrameTemplate.tex','r')
+    frame_template = Template(frame_template_file.read())
 
-line = sys.stdin.readline()
-columns = map(str.strip, line.split(','))
+    line = sys.stdin.readline()
+    columns = map(str.strip, line.split(','))
 
-task_lines = sys.stdin.readlines()
-grouped_lines = defaultdict(list)
+    task_lines = sys.stdin.readlines()
+    grouped_lines = defaultdict(list)
 
-for line in task_lines:
-    line_split = line.split(',')
-    grouped_lines[line_split[0]].append(line_split)
+    for line in task_lines:
+        line_split = line.split(',')
+        grouped_lines[line_split[0]].append(line_split)
 
-color = 0
-for name_story in grouped_lines:
-    make_story(name_story, 
-            grouped_lines[name_story], 
-            color_list[color], 
-            columns, 
-            frame_template)
+    color = 0
+    for name_story in grouped_lines:
+        make_story(name_story, 
+                grouped_lines[name_story], 
+                color_list[color], 
+                columns, 
+                frame_template)
 
-    color = (color + 1) % len(color_list)
+        color = (color + 1) % len(color_list)
+
+
+if __name__ == '__main__':
+    main()
