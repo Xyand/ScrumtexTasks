a0868d3985e49695062c8f0d230af31e8322a4c7
Xyand <xyand@fogofcode.com>
2012-07-08 16:27:46 +0300

    Refactored to class

--- MakeTasksTex.py
+++ MakeTasksTex.py
@@ -20,44 +20,71 @@ def expand_itemize(field):
     return '\\begin{{itemize}}\n{}\\end{{itemize}}\n' \
             .format(list_delim + list_delim.join(items))
 
-# TODO: Make it a class
-def make_story(name_story, tasks, color, columns, template):
-    ''' Create a tex file for a specific user story given its name tasks and color '''
 
-    name_folder = sys.argv[1]
-    if not isdir(name_folder): 
-        mkdir(name_folder)
-    tex_out = open('{}/{}.tex'.format(name_folder, name_story), 'w');
+class ScrumtexBuilder:
 
-    # Print header
-    file_header = open('TaskHeader.tex', 'r')
-    tex_out.writelines(file_header)
-    # Choose color
-    tex_out.write(color_command.format(color=color))
+    def __init__(self, columns, folder_out, file_header, file_template):
+        self.folder_out = folder_out
+        if not isdir(folder_out): 
+            mkdir(folder_out)
 
-    # Begin document
-    tex_out.write('\\begin{document}\n')
+        # Set header
+        file_header = open(file_header, 'r');
+        self.header = file_header.read();
 
-    # Render tasks
-    for fields in tasks:
-        assert len(fields) == len(columns), 'Field number mismatch: ' + str(len(fields))
-        fields = map(expand_itemize, fields)
-        tex_out.write(template.safe_substitute(dict(zip(columns, fields))))
+        self.columns = columns
+        self.color = 0
 
-    # Close document
-    tex_out.write('\\end{document}\n')
-    tex_out.close()
+        # Read frame template
+        file_template_frame = open(file_template,'r')
+        self.template_frame = Template(file_template_frame.read())
+ 
+    def set_color(self):
+        self.tex_out.write(color_command.format(color=color_list[self.color]))
+        self.color = (self.color + 1) % len(color_list)
+
+    def set_header(self):
+        self.tex_out.write(self.header)
+
+    def begin_document(self):
+        self.tex_out.write('\\begin{document}\n')
+
+    def end_document(self):
+        self.tex_out.write('\\end{document}\n')
+        self.tex_out.close()
+
+    def make_story(self, name_story, tasks):
+        ''' Create a tex file for a specific user story given its name tasks and color '''
+
+        self.tex_out = open('{}/{}.tex'.format(self.folder_out, name_story), 'w');
+
+        self.set_header()
+        self.set_color()
+        self.begin_document()
+
+        # Render tasks
+        for fields in tasks:
+            assert len(fields) == len(self.columns), \
+                'Field number mismatch: ' + str(len(fields))
+            fields = map(expand_itemize, fields)
+            fields_cols = dict(zip(self.columns, fields))
+            self.tex_out.write(self.template_frame.safe_substitute(fields_cols))
+
+        # Close document
+        self.end_document()
 
 def main():
     ''' Create all user story tex files '''
 
-    # Read frame template
-    frame_template_file = open('./FrameTemplate.tex','r')
-    frame_template = Template(frame_template_file.read())
-
     # Read column names
     line = sys.stdin.readline()
     columns = map(str.strip, line.split(','))
+
+    # Initialize tex builder
+    tex_builder = ScrumtexBuilder(columns, \
+            sys.argv[1], \
+            './TaskHeader.tex', \
+            './FrameTemplate.tex')
 
     # Split tasks to user stories
     grouped_lines = defaultdict(list)
@@ -68,14 +95,7 @@ def main():
     # Crate tex file for every user story
     color = 0
     for name_story in grouped_lines:
-        make_story(name_story, 
-                grouped_lines[name_story], 
-                color_list[color], 
-                columns, 
-                frame_template)
-
-        color = (color + 1) % len(color_list)
-
+        tex_builder.make_story(name_story, grouped_lines[name_story])
 
 if __name__ == '__main__':
     main()
